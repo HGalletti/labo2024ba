b4b3205639019e50035784b367d2d62bf4edbac7
HGalletti <mascossastre.jw@gmail.com@gmail.com>
2024-10-14 02:58:58 +0000

    auto commit 8       20241014 025855

--- src/workflows/929_workflow_SEMI_f202109_viv_ver_1.r
+++ src/workflows/929_workflow_SEMI_f202109_viv_ver_1.r
@@ -274,26 +274,26 @@ TS_strategy_base9 <- function( pinputexps )
   param_local$final_train$training <- c(
     202107, 202106, 202105, 202104, 202103, 202102, 202101, 
     202012, 202011, 202010, 202009, 202008, 202007, 
-    # 202006  Excluyo por variables rotas
+    202006,#Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
-    201912, 201911,
-    # 201910 Excluyo por variables rotas
-    201909, 201908, 201907, 201906,
-    # 201905  Excluyo por variables rotas
-    201904, 201903
+    201912, 201911, 
+    201910, #Excluyo por variables rotas
+    201909, 201908, 201907 #, 201906,
+    #201905,#  Excluyo por variables rotas
+    #201904#, 201903
   )
 
 
   param_local$train$training <- c(
     202105, 202104, 202103, 202102, 202101, 
     202012, 202011, 202010, 202009, 202008, 202007,
-    # 202006  Excluyo por variables rotas
+    202006, #  Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
     201912, 201911,
-    # 201910 Excluyo por variables rotas
+    201910,# Excluyo por variables rotas
     201909, 201908, 201907, 201906,
-    # 201905  Excluyo por variables rotas
-    201904, 201903, 201902, 201901
+    201905#  Excluyo por variables rotas
+    #201904, 201903, 201902, 201901
     )
 
   param_local$train$validation <- c(202106)
@@ -302,7 +302,7 @@ TS_strategy_base9 <- function( pinputexps )
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersamplin
-  param_local$train$undersampling <- 0.2
+  param_local$train$undersampling <- 0.4
   param_local$train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
 
   return( exp_correr_script( param_local ) ) # linea fija
